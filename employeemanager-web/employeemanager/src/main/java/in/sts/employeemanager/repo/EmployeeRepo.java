f565d28fb65ef058547b0bf3437a10867ae3800c
gautamkumar11 <kumargautam11gk@gmail.com>
2022-02-22 14:14:02 +0530

    added coments to the backend code

--- employeemanager-web/employeemanager/src/main/java/in/sts/employeemanager/repo/EmployeeRepo.java
+++ employeemanager-web/employeemanager/src/main/java/in/sts/employeemanager/repo/EmployeeRepo.java
@@ -6,9 +6,25 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import in.sts.employeemanager.model.Employee;
 
+/* 
+ * Repository for employee entity
+ *
+ * @author gautam kumar
+*/
 public interface EmployeeRepo extends JpaRepository<Employee, Long>{
 
+	/**
+	 * Delete employee by id.
+	 * 
+	 * @param employeeId
+	 */
 	void deleteEmployeeById(Long id);
 	
+	/**
+	 * Find employee by id.
+	 * 
+	 * @param employeeId
+	 * @return
+	 */
 	Optional<Employee> findEmployeeById(Long id);
 }
--- employeemanager-web/employeemanager/src/main/java/in/sts/employeemanager/service/EmployeeService.java
+++ employeemanager-web/employeemanager/src/main/java/in/sts/employeemanager/service/EmployeeService.java
@@ -5,19 +5,54 @@ import javax.transaction.Transactional;
 import org.springframework.stereotype.Service;
 import in.sts.employeemanager.model.Employee;
 
-
+/**
+* @author Gautam Kumar
+* 
+* Employee service interface to define all available methods for employee entity management
+*/
 @Service
 @Transactional
 public interface EmployeeService {
 
+	/*
+	 * Save employee details for registration
+	 * 
+	 * @param employee
+	 * 
+	 * @return
+	 */
 	public Employee addEmployee(Employee employee);
 	
+	/*
+	 * Fetch all employee list
+	 * 
+	 * @return
+	 */
 	public List<Employee> findAllEmployees();
 	
+	/*
+	 * Edit or update Employee details
+	 * 
+	 * @param employee
+	 * 
+	 * @return
+	 */
 	public Employee updateEmployee(Employee employee);
 	
+	/*
+	 * Find or get employee details by employee Id
+	 * 
+	 * @param id
+	 * 
+	 * @return
+	 */
 	public Employee findEmployeeById(Long id);
 	
+	/*
+	 * Delete Employee by their employee-id
+	 * 
+	 * @param id
+	 */
 	public void deleteEmployeeById(Long id);
 	
 }
